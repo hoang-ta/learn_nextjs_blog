4907677bb9f7733c6217638e74893b2c25aac7ac
Huy Hoang Ta <huyhoang.ta97@gmail.com>
2023-08-23 23:34:56 +0700

    css module

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,25 +2,28 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() => {
-          console.log(window.FB);
-        }}
-      />
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">← Back to home</Link>
-        <Image src={"/image/image1.png"} width={500} height={200} />
-      </h2>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() => {
+            console.log(window.FB);
+          }}
+        />
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">← Back to home</Link>
+          <Image src={"/image/image1.png"} width={500} height={200} />
+        </h2>
+      </Layout>
     </>
   );
 }
