1fde6b84e09f8b8bd9fc31d7ca97a184be680cd9
Huy Hoang Ta <huyhoang.ta97@gmail.com>
2023-08-23 23:31:42 +0700

    script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
@@ -8,6 +9,13 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log(window.FB);
+        }}
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">← Back to home</Link>
